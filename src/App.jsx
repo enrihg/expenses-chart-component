facb9a011a6cf2a12c82f0e1b00ccede166fd18d
enrihg <99612570+enrihg@users.noreply.github.com>
2024-01-30 19:57:59 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2>My balance</h2>
+        <span>$921.48</span>
+      </div>
+      <div>
+        <h1>Spending - Last 7 days</h1>
+      </div>
+      <ul>
+        <li>mon</li>
+        <li>tue</li>
+        <li>wed</li>
+        <li>thu</li>
+        <li>fri</li>
+        <li>sat</li>
+        <li>sun</li>
+      </ul>
+      <h3>Total this month</h3>
+      <span>$478.33</span>
+      <span>+2.4%</span>
+      <h3>from last month</h3>
+    </>
+  )
+}
+
+export default App
